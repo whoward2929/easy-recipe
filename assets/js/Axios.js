67319a77b96787f58b6af3d425f5649a12f1a97e
ericjcili <ericjcili@gmai.com>
2021-11-21 21:35:56 -0500

    added backend MONGO

--- assets/js/Axios.js
+++
@@ -1,5 +0,0 @@
-import Axios from "axios";
-
-export const instance = Axios.create({
-    baseURL: 'https://spoonacular.com/food-api'
-});
--- assets/js/config.js
+++
@@ -1,4 +0,0 @@
-const recipeAPIKey = "fa661c92f5cd4d209d501fc31b804565"
-
-const recipeAPIHost = "https://spoonacular.com";
-const recipeAPISearch = "https://spoonacular.com/food-api";
